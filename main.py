8337e87808cb12b4a989185c571de24fdf9a2144
mukund jajadiya <mukundjajadiya2017@gmail.com>
2022-04-08 13:42:59 +0530

    some change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from extract_text_from_video import ExtractTextFromVideo
 
 if __name__ == "__main__":
-    videoPath = "video/video2.mp4"
+    videoPath = "video/testVideo.mp4"
     ExtractTextFromVideo(videoPath)
